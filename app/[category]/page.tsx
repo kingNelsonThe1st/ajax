d1813f57ca0fb770a5e5b8e6d16585e9007fe234
nelsonthe1st <nelsonokonkwo218@gmail.com>
2025-01-28 03:54:12 -0800

    vercel

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -101,14 +101,15 @@ export default async function CategoryPage({
 }: {
   params: { category: string };
 }) {
-  const data: simplifiedProduct[] = await getData(params.category);
+  const category = await params.category; // Ensure category resolves correctly if it's Promise-like
+  const data: simplifiedProduct[] = await getData(category);
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Our Products for {params.category}
+            Our Products for {category}
           </h2>
         </div>
 
